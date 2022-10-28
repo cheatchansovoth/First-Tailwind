e4144ae550b6699cddaabac09b80927151451226
cheatchansovoth <57830826+cheatchansovoth@users.noreply.github.com>
2022-10-28 03:06:55 -0700

    Update App.js

--- TailwindProject/login/src/App.js
+++ TailwindProject/login/src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center min-w-full bg-[#073B39]">
-      <div className='login-form sm:flex sm:flex-row items-center justify-start rounded-l-2xl w-[70%] sm:bg-[#253631]'>
+    <div className="min-h-screen flex items-center justify-center min-w-full bg-[#073B39] ">
+      <div className='login-form sm:flex sm:flex-row  items-center justify-start rounded-l-2xl w-[70%] sm:bg-[#253631]'>
         <div className='image hidden md:block '>
           <img src='https://burst.shopifycdn.com/photos/elaborate-building-interior-with-light-teal-walls.jpg?width=1200&format=pjpg&exif=1&iptc=1' className='rounded-l-3xl h-[720px]'></img>
         </div>
-        <div className='text-login mx-[20%]'>
+        <div className='text-login m-auto'>
           <p className='sm:text-5xl text-2xl font-serif font-bold my-2 underline text-center text-[#6F6658]'>LOGIN</p>
           <p className='text-center my-1 text-[#6F6658]'>We keep it secret</p> 
           <form className='flex flex-col'>
